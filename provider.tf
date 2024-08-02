a4840f6ce9fe9a853024820cd6e2dca283d17c7f
Aboakye2074 <70584084+Aboakye2074@users.noreply.github.com>
2024-08-02 15:37:44 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,15 @@
-provider "aws" {
-  region = var.AWS_REGION
-
-  default_tags {
-    tags = {
-      lab = "vpn"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
+
+provider "aws" {
+  region = "us-east-2"
+}
+
